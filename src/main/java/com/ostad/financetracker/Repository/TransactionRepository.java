f2a35f14477c0279833f701b65a87881a37bfcc8
MD. SAIFUL ISLAM <42407511+ocpsaifulislam@users.noreply.github.com>
2026-02-20 06:49:00 +0600

    first commit

--- src/main/java/com/ostad/financetracker/Repository/TransactionRepository.java
+++ src/main/java/com/ostad/financetracker/Repository/TransactionRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public class TransactionRepository {
-    // The required in-memory List store
+
     private final List<Transaction> transactions = new ArrayList<>();
 
     public List<Transaction> findAll() {
